19b3b05d9d21b9f42a59bf434d47914231328e3e
DMITRIY ZHARSKIY <zharsky@MacBook-Pro.local>
2016-04-06 23:31:50 +0300

    Add ext for css-modules

--- component_css_loader.js
+++ component_css_loader.js
@@ -6,16 +6,20 @@ module.exports = function(source) {
 
   var query = loaderUtils.parseQuery(this.query);
   var ext = query.ext || 'styl';
+  var isCssModules = query.cssModules;
 
   var componentFileName = this.resourcePath.match(/[^\/]+$/)[0];
   var componentExt = componentFileName.match(/\.(.+)$/)[1];
   var styleFileName = componentFileName.replace(componentExt, ext);
   var stylePath = this.resourcePath.replace(componentExt, ext);
+  console.log(query);
 
   try {
     var stats = fs.statSync(stylePath);
     if (stats.isFile()) {
-      return "require('./" + styleFileName + "');\n" + source;
+      var requirePartString = isCssModules ? "var styles = require('./" : "require('./";
+
+      return requirePartString + styleFileName + "');\n" + source;
     }
   } catch(e) {}
 
